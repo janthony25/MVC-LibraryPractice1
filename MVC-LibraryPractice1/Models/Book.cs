93ffaf87dbad89ef1edada838e2cbeca2c0396ca
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 17:50:57 +1300

    Add project files.

---
+++ MVC-LibraryPractice1/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_LibraryPractice1.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+
+        public string CallNumber { get; set; }
+    }
+}
