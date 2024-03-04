ffe561cec487162ce2fdeb311f32c5e5566f0f2a
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 20:36:22 +1300

    add scope to program.cs

---
+++ MVC-LibraryPractice1/Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using MVC_LibraryPractice1.Data;
+
+namespace MVC_LibraryPractice1.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryPractice1Context(
+                serviceProvider.GetRequiredService<DbContextOptions<
+                    LibraryPractice1Context>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+
+                context.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "TDG 545" },
+                    new Book { Title = "Book 2", CallNumber = "PRD 401" }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MVC-LibraryPractice1/Program.cs
+++ MVC-LibraryPractice1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVC_LibraryPractice1.Data;
+using MVC_LibraryPractice1.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LibraryPractice1Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryPractice1Context") ?? throw new InvalidOperationException("Connection string 'LibraryPractice1Context' not found.")));
@@ -16,6 +17,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
 }
 
 app.UseHttpsRedirection();
